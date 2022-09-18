799602536fc8b1d71401f234bc3e416538cac86b
hiroshi <s20840011@gmail.com>
2022-09-18 10:41:58 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Layout from '../components/layout/layout'
+import { Provider } from 'next-auth/client'
+// import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
 }
 
 export default MyApp
